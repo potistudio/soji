8450537ed036e2c73acc408c2c375992914d7d82
POTI <potistudio@proton.me>
2026-01-18 03:08:06 +0900

    feat: Dummy progress bar

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 use colored::*;
+use indicatif::{ProgressBar, ProgressStyle};
+use std::thread;
+use std::time::Duration;
 
 const BANNER: &str = r#"
  _   _
@@ -10,4 +13,19 @@ const BANNER: &str = r#"
 fn main() {
     // Print the banner
     println!("{}", BANNER.bright_cyan());
+
+    let progress_bar = ProgressBar::new_spinner();
+    progress_bar.set_style(
+        ProgressStyle::default_spinner()
+            .tick_strings(&["⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"])
+            .template("{spinner:.green} {msg}")
+            .expect("Failed to set progress bar style"),
+    );
+    progress_bar.set_message("Analyzing...");
+    progress_bar.enable_steady_tick(Duration::from_millis(100));
+
+    // Simulate work
+    thread::sleep(Duration::from_secs(3));
+
+    progress_bar.finish_with_message("Done!");
 }
