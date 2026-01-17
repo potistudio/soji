2fcc67b5124f4f6ffb6be91e390a6f7e5ed5706f
POTI <potistudio@proton.me>
2026-01-18 03:07:53 +0900

    feat: Print banner

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use colored::*;
+
+const BANNER: &str = r#"
+ _   _
+( `-._)    s o j i
+ `  / \    ----------------------------
+(_/ \_/    Bring "Zen" to your machine.
+"#;
+
 fn main() {
-    println!("Hello, world!");
+    // Print the banner
+    println!("{}", BANNER.bright_cyan());
 }
